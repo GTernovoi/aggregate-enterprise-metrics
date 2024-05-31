71d6394fc46755d43e52952f7d46c9179f181fe8
GTvoi <157132029+GTernovoi@users.noreply.github.com>
2024-05-31 11:21:51 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+
+
+function main() {
+    const data = [];
+    const data = generateRandomData();
+}
+    }
+    for (let i = 0; i < 10; i++) {
+
+function generateRandomData() {
+    data.forEach(item => {
+main();
+    return data;
+}
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
